4a7059c9730ced490e49976289ed8f7d4060fa64
donna <donnacamos88@gmail.com>
2019-10-23 21:38:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+
+}
+
+function createEmployeeRecords() {
+
+}
+
+function createTimeInEvent() {
+
+}
+
+function hoursWorkedOnDate() {
+
+}
+
+function wagesEarnedOnDate() {
+
+}
+
+function allWageFor() {
+
+}
+
+function findEmployeeByFirstName() {
+
+}
+
+function calculatePayroll() {
+
+}
