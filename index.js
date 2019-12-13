67a0524376b9ca24811b41a2be0be10d8340bc69
Mark Daniel  Delgado <markpnoi26@gmail.com>
2019-12-13 00:08:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-// Your code here
+function createEmployeeRecord(array){
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+};
+
+
+function createEmployeeRecords(arrayOfArrays) {
+  return arrayOfArrays.map(employeeArray => {
+    return createEmployeeRecord(employeeArray)
+  });
+};
+
+function createTimeInEvent(employeeObject, dateStamp){
+  let timeIn = employeeObject.timeInEvents;
+  timeIn.push({
+    type: "TimeIn",
+    hour: parseInt(dateStamp.split(" ")[1], 10),
+    date: dateStamp.split(" ")[0]
+  });
+  return employeeObject;
+};
+
+function createTimeOutEvent(employeeObject, dateStamp){
+  let timeIn = employeeObject.timeOutEvents;
+  timeIn.push({
+    type: "TimeOut",
+    hour: parseInt(dateStamp.split(" ")[1], 10),
+    date: dateStamp.split(" ")[0]
+  });
+  return employeeObject;
+};
+
+function hoursWorkedOnDate(employeeObject, dateForm){
+  let hoursWorked;
+  let timeIn = employeeObject.timeInEvents.find(object =>{
+    return object.date === dateForm;
+  });
+  let timeOut = employeeObject.timeOutEvents.find(object =>{
+    return object.date === dateForm;
+  });
+  return ((timeOut.hour - timeIn.hour)/100);
+};
+
+function wagesEarnedOnDate(employeeObject, dateForm){
+  let hours = hoursWorkedOnDate(employeeObject, dateForm);
+  let wages = hours * employeeObject.payPerHour
+  return wages
+};
+
+function allWagesFor(employeeObject){
+  let allTimeOuts = [];
+  employeeObject.timeOutEvents.map(event => {
+    allTimeOuts.push(event.date);
+  })
+  let allWages = allTimeOuts.map(dateForm => {
+    return wagesEarnedOnDate(employeeObject, dateForm)
+  });
+  let accumulatedWage
+  allWages.reduce(wage => {
+    array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+  })
+};
