e3e05e233ddea2e73209aeb11eb63f6b6ca6640b
Kristinamarih <kristinahorensky@gmail.com>
2020-01-10 18:08:48 -0800

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function createEmployeeRecord(array) {
-    const obj = {}
-    return array.forEach((item, index) => {
-        obj[index] = item;
+const createEmployeeRecord = (array) => {
+    let obj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return obj
+};
+
+const createEmployeeRecords = (array) => {
+    let newArray = array.map(function(employee) {
+        return createEmployeeRecord(employee)
     });
-}
+    return newArray
+};
