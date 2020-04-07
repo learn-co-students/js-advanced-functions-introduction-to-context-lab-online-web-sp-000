a63a9666bf8582404e62db7898347857bffdec06
Malcolm <echelon1089@gmail.com>
2020-04-07 14:22:19 -0400

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,37 @@ function createEmployeeRecords(employee){
 }
 
  function createTimeInEvent(record, date){
-    let date = date.split(" ")[0]   
-    
-    return record 
+    let day = date.split(" ")[0]   
+    let hour = parseInt( date.split(" ")[1])
+    record.timeInEvents.push({type:"TimeIn", hour: hour, date: day})
+    return record
  }
+
+ function createTimeOutEvent(record, date){
+    let day = date.split(" ")[0]   
+    let hour = parseInt( date.split(" ")[1])
+    record.timeOutEvents.push({type:"TimeOut", hour: hour, date: day})
+    return record
+ }
+
+ function hoursWorkedOnDate(record, date){
+     let hourIn = record.timeInEvents.find(f => f.date === date )
+     let hourOut = record.timeOutEvents.find(f => f.date === date)
+    return ( hourOut.hour - hourIn.hour)/100
+ }
+
+ function wagesEarnedOnDate(record, date){
+     let hours = hoursWorkedOnDate(record, date)
+     return hours * record.payPerHour
+ }
+
+ function allWagesFor(record){
+    
+
+ }
+
+
+
+
+
+
