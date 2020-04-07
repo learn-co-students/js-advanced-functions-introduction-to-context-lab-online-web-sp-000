3bec4ee0a1defe4cc8bc535d1d8ef3c7835f8f76
Malcolm <echelon1089@gmail.com>
2020-04-06 21:37:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function createEmployeeRecord(employee){
+
+    let employeePayRollInfo ={}
+
+        employeePayRollInfo.firstName = employee[0]
+        employeePayRollInfo.familyName = employee[1]
+        employeePayRollInfo.title = employee[2]
+        employeePayRollInfo.payPerHour = employee[3]
+        employeePayRollInfo.timeInEvents = []
+        employeePayRollInfo.timeOutEvents = []
+
+         return employeePayRollInfo
+}
+
+function createEmployeeRecords(employee){
+    return employee.map( e => createEmployeeRecord(e))
+}
+
+ function createTimeInEvent(record, date){
+    let day = date.split(" ")[0]   
+    
+    return record 
+ }
