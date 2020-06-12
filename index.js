0a0bca080d56473ae766cbfe18d3d39d2ccde81c
lukesherwood <luke.sherwood55@gmail.com>
2020-06-12 13:00:43 +1200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function createEmployeeRecord(array, timeIn, timeOut){
+    let object = {}
+    object.firstName =array[0]
+    object.familyName = array[1]
+    object.title = array[2]
+    object.payPerHour = array[3]
+    object.timeInEvents = timeIn || []
+    object.timeOutEvents = timeOut || []
+    return object
+}
+
+function createEmployeeRecords(array) {
+    let records = array.map(element => createEmployeeRecord(element))
+    return records
+}
+
+function createTimeInEvent (record, time){
+    createEmployeeRecord(record, time)
+}
