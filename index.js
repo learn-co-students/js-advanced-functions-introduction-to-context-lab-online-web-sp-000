6812a733ca956678f12c8afdc0daa6ac02c1266d
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-29 21:00:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+let createEmployeeRecord = function(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(array) {
+    return array.map(function(employee){
+        return createEmployeeRecord(employee)
+    }) 
+}
+
+let createTimeInEvent = function(record, datestamp) {
+    let [date, hour] = datestamp.split(' ')
+    record.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return record
+}
+
+let createTimeOutEvent = function(record, datestamp) {
+    let [date, hour] = datestamp.split(' ')
+    record.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return record
+}
+
+let hoursWorkedOnDate = function(employee, soughtDate){
+    let inEvent = employee.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    let outEvent = employee.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
