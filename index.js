d7c46f957e2ac7686e3be783c47249b86f9b98dd
connorworthen <connorw220@protonmail.com>
2020-09-13 08:03:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-function createEmployeeRecord() {}
+function createEmployeeRecord(record) {
+  let clockIn = [];
+  let clockOut = [];
+  let details = {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: clockIn,
+    timeOutEvents: clockOut,
+  };
+  return details;
+}
+
+function createEmployeeRecords(records) {
+  const arr = records.map(createEmployeeRecord);
+  return arr;
+}
+
+function createTimeInEvent(record, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  record.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date: date,
+  });
+  return record;
+}
+
+function createTimeOutEvent(record, dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  record.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date,
+  });
+  return record;
+}
+
+function hoursWorkedOnDate(record, date) {
+  let hours = 0;
+  let clockIn = record.timeInEvents.find(function (key) {
+    return key.date === date;
+  });
+  let clockOut = record.timeOutEvents.find(function (key) {
+    return key.date === date;
+  });
+  hours = (clockOut.hour - clockIn.hour) / 100;
+  return hours;
+}
