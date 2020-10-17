9b32e2b1b87cc3657aedddd92ddec4dca8e76735
carrollm2 <carrollm2@gmail.com>
2020-10-17 13:24:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+function createEmployeeRecord(testEmployee){
+
+    let createdEmployee = {}
+    let employeeFields = ['firstName', 'familyName', 'title', 'payPerHour']
+
+    employeeFields.map(function(employeeField, value){
+        createdEmployee[employeeField] = testEmployee[value]
+    });   
+
+    createdEmployee['timeInEvents'] = []
+    createdEmployee['timeOutEvents'] = []
+
+    return createdEmployee
+
+}
