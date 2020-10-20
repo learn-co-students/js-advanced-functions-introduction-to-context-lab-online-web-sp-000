5779920eb3319aeddd9f2f751811bccebf31f865
carrollm2 <carrollm2@gmail.com>
2020-10-19 23:38:15 -0400

    Done.

--- index.js
+++ index.js
@@ -26,13 +26,37 @@ function createEmployeeRecords(employeeArrays){
     return employeeRecords
 }
 
+//
+function createTimeEvent(timeDateStamp, event){
+
+    let timeEventObject = {}
+    let timeEventFields = ['date', 'hour']
+    let timeDateStampArray = timeDateStamp.split(" ")
+
+    timeEventFields.map(function(timeEventField, value){
+        if (timeEventField === 'hour'){
+            timeEventObject[timeEventField] = parseInt(timeDateStampArray[value])
+        } else {
+            timeEventObject[timeEventField] = timeDateStampArray[value]
+        }
+    });       
+
+    if (event === "Out"){
+        timeEventObject['type'] ="TimeOut"
+    }
+
+    else if (event === "In"){
+        timeEventObject['type'] ="TimeIn"
+    }
+
+    return timeEventObject
+}
+//
+
+
 
 function createTimeInEvent(employee, timeInDateStamp){
-    let timeInObject = {}
-    timeInObject['type'] ="TimeIn"
-
-    timeInObject['date'] = timeInDateStamp.split(" ")[0]
-    timeInObject['hour'] = parseInt(timeInDateStamp.split(" ")[1])
+    let timeInObject = createTimeEvent(timeInDateStamp, "In")
 
     employee.timeInEvents.push(timeInObject)
     
@@ -40,13 +64,40 @@ function createTimeInEvent(employee, timeInDateStamp){
 }
 
 function createTimeOutEvent(employee, timeOutDateStamp){
-    let timeOutObject = {}
-    timeOutObject['type'] ="TimeOut"
-
-    timeOutObject['date'] = timeOutDateStamp.split(" ")[0]
-    timeOutObject['hour'] = parseInt(timeOutDateStamp.split(" ")[1])
+    let timeOutObject = createTimeEvent(timeOutDateStamp, "Out")
 
     employee.timeOutEvents.push(timeOutObject)
     
     return employee
 }
+
+
+
+
+
+
+
+
+// function createTimeInEvent(employee, timeInDateStamp){
+//     let timeInObject = {}
+//     timeInObject['type'] ="TimeIn"
+
+//     timeInObject['date'] = timeInDateStamp.split(" ")[0]
+//     timeInObject['hour'] = parseInt(timeInDateStamp.split(" ")[1])
+
+//     employee.timeInEvents.push(timeInObject)
+    
+//     return employee
+// }
+
+// function createTimeOutEvent(employee, timeOutDateStamp){
+//     let timeOutObject = {}
+//     timeOutObject['type'] ="TimeOut"
+
+//     timeOutObject['date'] = timeOutDateStamp.split(" ")[0]
+//     timeOutObject['hour'] = parseInt(timeOutDateStamp.split(" ")[1])
+
+//     employee.timeOutEvents.push(timeOutObject)
+    
+//     return employee
+// }
