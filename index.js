073a432892cb2c69efa7ad75ad0b53c7b1521fdf
carrollm2 <carrollm2@gmail.com>
2020-10-19 23:18:50 -0400

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,38 @@ function createEmployeeRecord(testEmployee){
     return createdEmployee
 
 }
+
+
+function createEmployeeRecords(employeeArrays){
+    let employeeRecords = [];
+    employeeArrays.map(function(employeeArray){
+        employeeRecords.push(createEmployeeRecord(employeeArray))
+    });
+
+    return employeeRecords
+}
+
+
+function createTimeInEvent(employee, timeInDateStamp){
+    let timeInObject = {}
+    timeInObject['type'] ="TimeIn"
+
+    timeInObject['date'] = timeInDateStamp.split(" ")[0]
+    timeInObject['hour'] = parseInt(timeInDateStamp.split(" ")[1])
+
+    employee.timeInEvents.push(timeInObject)
+    
+    return employee
+}
+
+function createTimeOutEvent(employee, timeOutDateStamp){
+    let timeOutObject = {}
+    timeOutObject['type'] ="TimeOut"
+
+    timeOutObject['date'] = timeOutDateStamp.split(" ")[0]
+    timeOutObject['hour'] = parseInt(timeOutDateStamp.split(" ")[1])
+
+    employee.timeOutEvents.push(timeOutObject)
+    
+    return employee
+}
