bec6154645c4a28457adc99b72f059bd67ab1d4b
carrollm2 <carrollm2@gmail.com>
2020-10-20 21:30:02 -0400

    Done.

--- index.js
+++ index.js
@@ -108,26 +108,3 @@ function allWagesFor(employeeRecord){
 
 
 
-// function createTimeInEvent(employee, timeInDateStamp){
-//     let timeInObject = {}
-//     timeInObject['type'] ="TimeIn"
-
-//     timeInObject['date'] = timeInDateStamp.split(" ")[0]
-//     timeInObject['hour'] = parseInt(timeInDateStamp.split(" ")[1])
-
-//     employee.timeInEvents.push(timeInObject)
-    
-//     return employee
-// }
-
-// function createTimeOutEvent(employee, timeOutDateStamp){
-//     let timeOutObject = {}
-//     timeOutObject['type'] ="TimeOut"
-
-//     timeOutObject['date'] = timeOutDateStamp.split(" ")[0]
-//     timeOutObject['hour'] = parseInt(timeOutDateStamp.split(" ")[1])
-
-//     employee.timeOutEvents.push(timeOutObject)
-    
-//     return employee
-// }
