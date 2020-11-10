5027d97d63ebab4db363cce17a6aa724427751c0
chienleow <chienleow1@gmail.com>
2020-11-10 14:39:39 -0500

    Done.

--- index.js
+++ index.js
@@ -47,12 +47,21 @@ let createEmployeeRecords = function(arrayOfArrays) {
  // find the timeInEvent object that matches the date from the argument
  // calculate the hours between timeIn and TimeOut
 
- let hoursWorkedOnDate = function(employeeRecord, dateStamp) {
+ function hoursWorkedOnDate(employeeRecord, dateStamp) {
+    // debugger;
     let employeeRecordDate = employeeRecord["timeInEvents"]["date"]
-    debugger;
+    console.log("HELLO!", employeeRecord["timeInEvents"][0]["date"])
     let foundDate = employeeRecordDate.find(object => object === dateStamp)
  }
 
  // incoming arrays
 // incoming object (1 employee record): {firstName: "Gray", familyName: "Worm", title: "Security", payPerHour: 1, timeInEvents: [{type: "TimeIn", hour: 1400, date: 2014-02-28}], timeOutEvents: []}
 // incoming dateStamp: "2014-02-28 1400"
+
+let wagesEarnedOnDate = function(employeeRecord, dateStamp) {
+
+}
+
+let allWagesFor = function(employeeRecord) {
+
+}
