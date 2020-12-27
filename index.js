92deb0f9ab625e74a2c50be7d6c19733384e4ce2
Jason J Cruz <jaycru06@yahoo.com>
2020-12-27 08:01:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+const createEmployeeRecord = (employeeRecord) => {
+  let newEmployeeRecord = {};
+  newEmployeeRecord.firstName = employeeRecord[0];
+  newEmployeeRecord.familyName = employeeRecord[1];
+  newEmployeeRecord.title = employeeRecord[2];
+  newEmployeeRecord.payPerHour = employeeRecord[3];
+  return newEmployeeRecord;
+}
