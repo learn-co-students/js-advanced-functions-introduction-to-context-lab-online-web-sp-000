74c3aae5bd45afc827b9ea7d7aa7c3ca38405b59
Jason J Cruz <jaycru06@yahoo.com>
2020-12-28 08:30:21 +0000

    Done.

--- index.js
+++ index.js
@@ -12,24 +12,48 @@ const createEmployeeRecord = (employeeRecord) => {
 
 // Process an Array of Arrays into an Array of employee records
 const createEmployeeRecords = (record) => {
-  // createEmployeeRecords
-  //       1) creates two records
-  //       2) correctly assigns the first names
-  //       3) creates more than 2 records
-  // let twoRows = [
-  //       ["moe", "sizlak", "barkeep", 2],
-  //       ["bartholomew", "simpson", "scamp", 3]
-  //     ]
-  // it("correctly assigns the first names", function () {
-  //       let employeeRecords = createEmployeeRecords(twoRows)
-  //       let nameExtractor = function (e) { return e.firstName }
-  //       expect(employeeRecords.map(nameExtractor)).to.eql(["moe", "bartholomew"]);
-  //     })
   let employeeRecords = record.map(employeeArray => {
     return {firstName: employeeArray[0]}
   })
   return employeeRecords;
 }
+
+// Adds a timeIn event obj to an employee's record of timeInEvents
+// when provided an employee record and Date/Time String and returns 
+// the updated record 
+const createTimeInEvent = (employeeRecord, dateTime) => {
+    employeeRecord.timeInEvents = new Array(1).fill({
+      type: "TimeIn", date: dateTime.split(" ")[0], hour: Number(dateTime.split(" ")[1])})
+    return employeeRecord
+}
+
+// Adds a timeOut event obj to an employee's record of timeOutEvents
+// when provided an employee record and Date/Time String and returns 
+// the updated record 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
