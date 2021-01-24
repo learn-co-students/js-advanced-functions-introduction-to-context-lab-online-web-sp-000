857f0e801bbcf58f584635bba9a1cb75f4ad9057
github username <github email address>
2021-01-24 11:34:50 -0600

    Done.

--- index.js
+++ index.js
@@ -42,6 +42,30 @@ function hoursWorkedOnDate(employee, date) {
     let punchIn = employee.timeInEvents.find(element => element.date === date);
     let punchOut = employee.timeOutEvents.find(element => element.date === date);
 
-    let hoursWorked = punchIn.hour - punchOut.hour
+    let hoursWorked = (punchOut.hour - punchIn.hour)/100
     return hoursWorked
+}
+
+function wagesEarnedOnDate(employee, date) {
+    let wagesOwed = hoursWorkedOnDate(employee, date) * employee.payPerHour
+    return wagesOwed
+}
+
+function allWagesFor(employee) {
+    let daysWorked = employee.timeOutEvents.map(day => day.date);
+    let wagesWorked = daysWorked.map(date => wagesEarnedOnDate(employee, date));
+    let totalWages = wagesWorked.reduce(((total, pay) => total + pay),0);
+    return totalWages;
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(arrEmployees) {
+    let wagesFor = function(total, pay) { 
+        return total + allWagesFor(pay)};
+    let allWages = arrEmployees.reduce(wagesFor, 0);
+    return allWages
 }
