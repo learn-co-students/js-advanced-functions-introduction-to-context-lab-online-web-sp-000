26df293c32fe504eb2e6b46c575df69c7bbadda9
learn-co-git <argus.two.2@protonmail.com>
2021-01-26 17:59:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Your code here
+let createEmployeeRecord = function(arr) {
+  return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(array) {
+  return array.map(ele => createEmployeeRecord(ele));
+}
+
+let createTimeInEvent = function(record, date) {
+    let [day, hour] = date.split(" ")
+
+    let timeInObj = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: day
+    }
+     record.timeInEvents.push(timeInObj);
+     return record;
+
+}
+
+let createTimeOutEvent = function(record, date) {
+  let [day, hour] = date.split(" ")
+
+  let timeOutObj = {
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date: day
+  }
+   record.timeOutEvents.push(timeOutObj);
+   return record;
+}
+
+let hoursWorkedOnDate = function(record, day) {
+  let time_In = record.timeInEvents.find(ele => ele.date == day)
+  let time_Out = record.timeOutEvents.find(ele => ele.date == day)
+  let hours = (time_Out.hour - time_In.hour)/100
+  return hours;
+}
+
+let wagesEarnedOnDate = function(record, day) {
+  return (hoursWorkedOnDate(record, day) * record.payPerHour);
+}
+
+let allWagesFor = function(record) {
+  let total = 0;
+  let day_total = record.timeInEvents.map(ele => wagesEarnedOnDate(record, ele.date));
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return day_total.reduce(reducer);
+}
+
+let calculatePayroll = function(array) {
+  const pay = (total, employee) => total + allWagesFor(employee)
+  return array.reduce(pay, 0);
+}
+
+let findEmployeeByFirstName = function(srcArr, firstName) {
+    return srcArr.find(record => record.firstName === firstName)
+}
