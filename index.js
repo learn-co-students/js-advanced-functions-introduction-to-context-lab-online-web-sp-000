976548c5cc51b053cf6742bf40db245c74001826
Catherine Gloria <catherinegloria@gmail.com>
2021-02-13 09:54:13 -0800

    create employee record function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(array) {
+  let newObj = {};
+  newObj.firstName = array[0];
+  newObj.familyName = array[1];
+  newObj.title = array[2];
+  newObj.payPerHour = array[3];
+  newObj.timeInEvents = [];
+  newObj.timeOutEvents = [];
+  return newObj
+}
