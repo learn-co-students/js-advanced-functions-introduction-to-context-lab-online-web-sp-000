b15237f257aee8a4d70824087e6781c9953e1bb2
Catherine Gloria <catherinegloria@gmail.com>
2021-02-13 22:07:44 -0800

    time in and time out functions

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function createEmployeeRecord(array) {
   newObj.timeOutEvents = [];
   return newObj
 }
+
+function createEmployeeRecords(arrOfArrays) {
+  let newArr = arrOfArrays.map(e => createEmployeeRecord(e))
+  return newArr
+}
+
+function createTimeInEvent(employeeObj, dateStamp) {
+  const newObj = {};
+  newObj.type = "TimeIn";
+  newObj.hour = parseInt(dateStamp.slice(11), 10);
+  newObj.date = dateStamp.slice(0, dateStamp.length - 5);
+
+  employeeObj.timeInEvents.push(newObj);
+  return employeeObj
+}
+
+function createTimeOutEvent(employeeObj, dateStamp) {
+  const newObj = {};
+  newObj.type = "TimeOut";
+  newObj.hour = parseInt(dateStamp.slice(11), 10);
+  newObj.date = dateStamp.slice(0, 11);
+
+  employeeObj.timeOutEvents.push(newObj);
+  return employeeObj
+}
